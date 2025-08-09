0bc50bd83dd57ede47173f9bfbbb96e398940c9b
Ivan <ivan.zderadicka@gmail.com>
2025-08-09 19:35:45 +0200

    Check if file is selected

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -3,7 +3,11 @@
   import { Input } from "$lib/components/ui/input/index.js";
   import { Label } from "$lib/components/ui/label/index.js";
   import { apiClient } from "$lib/api/client";
+  import { breadcrumb } from "$lib/globals.svelte";
 
+  breadcrumb.path = [{ name: "Upload Ebook", path: "/upload" }];
+
+  let fileSelected = $state(false);
   const id = $props.id();
   let form: HTMLFormElement;
 
@@ -12,6 +16,11 @@
     const formData = new FormData(form);
     const response = await apiClient.uploadFile(formData);
     console.log(response);
+  }
+
+  function handleFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    fileSelected = !!input.files && input.files?.length > 0; // Check if a file is selected
   }
 </script>
 
@@ -24,7 +33,12 @@
 >
   <div class="mb-4">
     <Label for="file-input-{id}">Upload a file</Label>
-    <Input type="file" name="file" id="file-input-{id}" />
-    <Button onclick={handleFileUpload}>Upload</Button>
+    <Input
+      type="file"
+      name="file"
+      id="file-input-{id}"
+      onchange={handleFileChange}
+    />
+    <Button onclick={handleFileUpload} disabled={!fileSelected}>Upload</Button>
   </div>
 </form>
