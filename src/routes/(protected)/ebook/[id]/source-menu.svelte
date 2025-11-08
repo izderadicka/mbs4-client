31f66104b59527df441ea80656a9474f65688241
Ivan <ivan.zderadicka@gmail.com>
2025-11-08 17:35:58 +0100

    Add source menu

---
+++ src/routes/(protected)/ebook/[id]/source-menu.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" module>
+  export type SourceMenuActions = "delete" | "convert";
+</script>
+
+<script lang="ts">
+  import MenuIcon from "@lucide/svelte/icons/ellipsis-vertical";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import type { EbookSource } from "$lib/api";
+  type Props = {
+    source: EbookSource;
+    onMenuSelected: (source: EbookSource, action: SourceMenuActions) => void;
+  };
+  let { source, onMenuSelected }: Props = $props();
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    {#snippet child({ props })}
+      <Button {...props} variant="ghost">
+        <MenuIcon class="" />
+      </Button>
+    {/snippet}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content align="end">
+    <DropdownMenu.Group>
+      <DropdownMenu.Label>Ebook File Actions</DropdownMenu.Label>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item onSelect={() => onMenuSelected(source, "delete")}
+        >Delete File</DropdownMenu.Item>
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
--- src/routes/(protected)/ebook/[id]/sources-list.svelte
+++ src/routes/(protected)/ebook/[id]/sources-list.svelte
@@ -5,8 +5,13 @@
   import DownloadIcon from "@lucide/svelte/icons/download";
   import { Button } from "$lib/components/ui/button";
   import { apiClient } from "$lib/api/client";
+  import SourceMenu from "./source-menu.svelte";
 
   let { sources }: { sources: EbookSource[] } = $props();
+
+  function onSourceMenuSelected(source: EbookSource, action: string) {
+    console.log("onSourceMenuSelected", source, action);
+  }
 </script>
 
 <Table.Root>
@@ -15,6 +20,7 @@
       <Table.Head class="w-[4rem]">Format</Table.Head>
       <Table.Head>Size</Table.Head>
       <Table.Head>Download</Table.Head>
+      <Table.Head>More ...</Table.Head>
     </Table.Row>
   </Table.Header>
   <Table.Body>
@@ -22,10 +28,13 @@
       <Table.Row>
         <Table.Cell class="font-medium">{source.format_extension}</Table.Cell>
         <Table.Cell>{prettyBytes(source.size)}</Table.Cell>
-        <Table.Cell class="truncate"
+        <Table.Cell class="w-3"
           ><Button href={apiClient.downloadUrl(source.location)} variant="link"
             ><DownloadIcon /></Button
           ></Table.Cell>
+        <Table.Cell class="w-3">
+          <SourceMenu {source} onMenuSelected={onSourceMenuSelected} />
+        </Table.Cell>
       </Table.Row>
     {/each}
   </Table.Body>
